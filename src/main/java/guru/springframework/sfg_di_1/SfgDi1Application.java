0bdf6cad4aa68cf0e6bcae9fa7ca4e8e84fa9fbf
Rick Fencl <rickf@powin.com>
2025-05-03 21:20:15 -0700

    update to latest spring boot

---
+++ src/main/java/guru/springframework/sfg_di_1/SfgDi1Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfg_di_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgDi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgDi1Application.class, args);
+	}
+
+}
