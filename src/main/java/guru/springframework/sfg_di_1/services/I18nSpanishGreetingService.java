e293b97f865329489511bcd9f6ff5e5a42adb53f
Rick Fencl <rickf@powin.com>
2025-05-05 22:47:11 -0700

    default profile example

--- src/main/java/guru/springframework/sfg_di_1/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfg_di_1/services/I18nSpanishGreetingService.java
@@ -3,7 +3,7 @@ package guru.springframework.sfg_di_1.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService  {
 
